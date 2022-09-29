6eb99b7f0c35dc1680929f2d8e5ff504d000cc83
rayhan18 <rabu4960@gmail.com>
2022-09-29 10:02:01 +0300

    auth functinality

--- src/components/OAuth.js
+++ src/components/OAuth.js
@@ -1,9 +1,43 @@
+import { getAuth, signInWithPopup } from 'firebase/auth';
 import React from 'react'
 import { FcGoogle } from "react-icons/fc";
-export default function OAuth() {
+import { toast } from 'react-toastify';
+import { GoogleAuthProvider } from "firebase/auth";
+import { db } from '../firebase';
+import { doc, getDoc, serverTimestamp, setDoc } from 'firebase/firestore';
+import { useNavigate } from 'react-router-dom';
 
+
+
+export default function OAuth() {
+  const navigate =useNavigate()
     async function onGoogleClick(){
         
+      try {
+        const auth = getAuth();
+        const provider = new GoogleAuthProvider();
+        const result = await signInWithPopup(auth , provider)
+        const user = result.user;
+      //  console.log(user)
+      //chack rof user
+      const docRef = doc(db ,'users', user.uid )
+      const docSnap =await getDoc(docRef)
+        if(!docSnap.exists()){
+          await setDoc(docRef ,{
+            name:user.displayName,
+            email:user.email,
+            timestamp:serverTimestamp()
+          }
+            
+            )
+        }
+
+       navigate('/')
+
+      } catch (error) {
+        
+        toast.error("you are not authoright")
+      }
     }
   return (
     <div>
